1158c1a7b01164b23bf08e031e109b3b2f342e79
joris-s-k <joris-s-k@users.noreply.github.com>
2023-06-30 03:06:30 +0200

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,3 +1,4 @@
+/* function was yoinked from dribbblish theme */
 function waitForElement(els, func, timeout = 100) {
     const queries = els.map(el => document.querySelector(el));
     if (queries.every(a => a)) {
@@ -7,11 +8,10 @@ function waitForElement(els, func, timeout = 100) {
     }
 }
 
-waitForElement([".main-navBar-entryPoints .GlueDropTarget:has(> .main-createPlaylistButton-button)"], ([nodeToBeRemoved]) => {
+waitForElement([".main-navBar-entryPoints .GlueDropTarget:has(> .main-createPlaylistButton-button)"], ([nodeToBeMoved]) => {		
+    nodeToBeMoved.parentNode.parentNode.insertBefore(nodeToBeMoved, nodeToBeMoved.parentNode);
 		
-    nodeToBeRemoved.parentNode.parentNode.insertBefore(nodeToBeRemoved, nodeToBeRemoved.parentNode);
-		
-    });
+});
 
 
 	
